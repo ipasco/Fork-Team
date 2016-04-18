21907a55aa8c727f2b1463410b2692f868755513
xacasi <isabel.pasco@obf.ateneo.edu>
2016-04-18 23:01:32 +0800

    Add PBPPI

---
+++ PBPPI/PBPPI/Form2.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace PBPPI
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        //    button1.TabStop = false;
+        //button1.FlatStyle = FlatStyle.Flat;
+        //button1.FlatAppearance.BorderSize = 0;
+        //button1.FlatAppearance.BorderColor = Color.FromArgb(0, 255, 255, 255);
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+    }
+}
